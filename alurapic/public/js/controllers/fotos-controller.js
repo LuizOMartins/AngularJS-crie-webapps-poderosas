f3a11006345e61cc86e9fdb3959b6ed13fa0aac3
Luiz Eduardo Martins Martins <1690342@TCS.com>
2020-07-11 00:39:09 -0300

    Inserindo fotos no Controller

---
+++ alurapic/public/js/controllers/fotos-controller.js
@@ -0,0 +1,7 @@
+angular.module('alurapic').controller('FotosController',function($scope){
+  
+    $scope.foto = {
+        titulo: 'Leao',
+        url: 'https://exame.com/wp-content/uploads/2019/08/gettyimages-1041987488.jpg'
+    }
+});
