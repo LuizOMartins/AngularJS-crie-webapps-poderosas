d5152058f11817aa11e9a9f03c4d1df344c6616d
Luiz Eduardo Martins Martins <1690342@TCS.com>
2020-07-11 10:29:03 -0300

    Inserindo Fotoso

--- alurapic/public/js/controllers/fotos-controller.js
+++ alurapic/public/js/controllers/fotos-controller.js
@@ -1,7 +1,19 @@
 angular.module('alurapic').controller('FotosController',function($scope){
   
-    $scope.foto = {
-        titulo: 'Leao',
+    $scope.foto1 = {
+        titulo: 'Leao 1',
         url: 'https://exame.com/wp-content/uploads/2019/08/gettyimages-1041987488.jpg'
     }
+
+    $scope.foto2 = {
+        titulo: 'Leao 2',
+        url: 'https://exame.com/wp-content/uploads/2019/08/gettyimages-1041987488.jpg'
+    }
+
+    $scope.foto3 = {
+        titulo: 'Leao 3',
+        url: 'https://exame.com/wp-content/uploads/2019/08/gettyimages-1041987488.jpg'
+    }
+
+
 });
