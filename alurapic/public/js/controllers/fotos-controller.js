075181db21bd85739d78a1066212bfeb9cd332d6
Luiz Eduardo Martins Martins <1690342@TCS.com>
2020-07-11 10:34:19 -0300

    Diretiva ng-repeat

--- alurapic/public/js/controllers/fotos-controller.js
+++ alurapic/public/js/controllers/fotos-controller.js
@@ -1,19 +1,17 @@
 angular.module('alurapic').controller('FotosController',function($scope){
   
-    $scope.foto1 = {
+    $scope.fotos = [
+        {
         titulo: 'Leao 1',
         url: 'https://exame.com/wp-content/uploads/2019/08/gettyimages-1041987488.jpg'
-    }
-
-    $scope.foto2 = {
-        titulo: 'Leao 2',
-        url: 'https://exame.com/wp-content/uploads/2019/08/gettyimages-1041987488.jpg'
-    }
-
-    $scope.foto3 = {
-        titulo: 'Leao 3',
-        url: 'https://exame.com/wp-content/uploads/2019/08/gettyimages-1041987488.jpg'
-    }
-
-
+        },
+        {
+            titulo: 'Leao 2',
+            url: 'https://exame.com/wp-content/uploads/2019/08/gettyimages-1041987488.jpg'
+        },
+        {
+            titulo: 'Leao 3',
+            url: 'https://exame.com/wp-content/uploads/2019/08/gettyimages-1041987488.jpg'
+        }
+    ]
 });
